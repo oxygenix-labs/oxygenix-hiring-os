c3eaff8b1938a62ad2966c59906220332c974e1d
Ankit Kumar <thankitkumar@gmail.com>
2026-01-08 20:27:12 +0530

    feat: add AIGenerationPanel component for job description generation options

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import LandingPage from "./pages/LandingPage";
+import Index from "./pages/Index";
+import HiringFlow from "./pages/HiringFlow";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Index />} />
+          <Route path="/hiring" element={<HiringFlow />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
